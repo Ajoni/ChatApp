85bd9a13b4b1f01804eb30f46a396136f5935899
Adam Sienkiewicz <adam584@onet.pl>
2018-02-07 10:40:09 +0100

    added some server logic

--- ChatApp/MainWindow.xaml.cs
+++ ChatApp/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatApp
 {
@@ -20,9 +22,98 @@ namespace ChatApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 100;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private static void SetupServer()
+        {
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            //Client connected, waiting for request...
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string[] text = Encoding.ASCII.GetString(recBuf).Split(' ');
+
+            //if (text[0].ToLower() == "/!hi") // Add client to connected lsit
+            //{
+            //    ConnectedList.Items.Add(text[1]);
+            //}
+            //else 
+            if (text[0].ToLower() == "/!exit") // Client wants to exit gracefully
+            {
+                // Always Shutdown before closing
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+            else    //send current client's msg to other clients
+            {
+                foreach (Socket sck in clientSockets)
+                {
+                    if (sck != current)
+                    {
+                        sck.Send(recBuf);
+                    }
+                }
+            }
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
         }
     }
 }
